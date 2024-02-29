9bdf9e3e259f8cdc3088325738ff5253e724dfb9
kritikpatidar <151845306+kritikpatidar@users.noreply.github.com>
2024-02-29 16:22:08 +0000

    new chaNGE

---
+++ Javascript/datatype.js
@@ -0,0 +1,18 @@
+let age = 18;
+let name= "Kritik";
+let isloggined = false;
+
+console.table([age,name,isloggined])
+/*
+number 2is to power 53 range is iske 
+bigint
+string= "name"
+boolean= true false
+null = black or empty
+undefiend = not defiend
+symbol= unique ness ke liye
+
+object
+
+*/
+console.log(typeof Kritik);
